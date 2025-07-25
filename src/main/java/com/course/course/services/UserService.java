e73e1c7890f6458b5c2ed002ad6a9ec9f3297682
Gabriela Pacheco Vieira <bela.vieira.pacheco@gmail.com>
2025-07-25 16:29:56 -0300

    User update

--- src/main/java/com/course/course/services/UserService.java
+++ src/main/java/com/course/course/services/UserService.java
@@ -24,13 +24,25 @@ public class UserService {
 		return obj.get();
 	}
 
-	// retornar o usuario salvo
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
 	}
 
 }
